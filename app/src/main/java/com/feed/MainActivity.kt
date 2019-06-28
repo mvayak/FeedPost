8e24a855b2ba06c190b3003358f63d0fec76ba1d
mvayak <info.mvayak@gmail.com>
2019-06-28 11:18:47 +0530

    Initial commit

--- app/src/main/java/com/feed/MainActivity.kt
+++ app/src/main/java/com/feed/MainActivity.kt
@@ -21,32 +21,61 @@ class MainActivity : AppCompatActivity() {
     private var tag = "story"
     private var ndPage = 0
     private var isLoadMoreData = false
+    private var isRefresh=false
     private var callRootBack: Call<RootModel>? = null
     private var hitModelList: MutableList<RootModel.HitModel> = mutableListOf()
     private var searchDataAdapter: SearchDataAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpToolbarTitle()
+        setToolbarTitle()
         initView()
         callSearchDataAPI()
     }
 
-    private fun setUpToolbarTitle() {
+    private fun setToolbarTitle() {
         supportActionBar!!.title = resources.getString(R.string.app_name)
     }
 
+    // Initialing View
+    private fun initView() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        swipeRefreshLayout.setOnRefreshListener {
+            callRootBack?.let {
+                it.cancel()
+            }
+            isRefresh=true
+            isLoadMoreData=false
+            page = 1
+
+            textViewNoInternet.visibility = View.GONE
+
+            callSearchDataAPI()
+        }
+
+    }
+    // Calling API
     private fun callSearchDataAPI() {
 
         callRootBack = RestClient(this).getApiService().searchData(tag, page)
         callRootBack?.enqueue(object : Callback<RootModel> {
             override fun onResponse(call: Call<RootModel>, response: Response<RootModel>) {
                 if (response.code() == AppConst.SUCCESS_CODE) {
-
+                    if(isRefresh){
+                        isRefresh=false
+                        hitModelList.clear()
+                        searchDataAdapter?.let {
+                            it.selectedItem.clear()
+                            it.setMoreDataAvailable(false)
+                            searchDataAdapter=null
+                            setToolbarTitle()
+                        }
+                    }
                     ndPage = response.body()?.nbPages!!
                     response.body()?.hits?.let {
                         hitModelList.addAll(it)
                     }
+
                     setUpSearchDataAdapter()
                 }
 
@@ -56,10 +85,9 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<RootModel>, t: Throwable) {
                 progressBarLoader.visibility = View.GONE
+                isRefresh=false
                 swipeRefreshLayout.isRefreshing = false
-                if (isConnected()) {
-                    resources.getString(R.string.someting_wrong).showToast(this@MainActivity)
-                } else {
+                if (!isConnected()) {
                     if (hitModelList.size > 0) {
                         resources.getString(R.string.no_internet_connection).showToast(this@MainActivity)
                         textViewNoInternet.visibility = View.GONE
@@ -76,10 +104,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+
+
+
     // Set Up RecyclerView Adapter
     private fun setUpSearchDataAdapter() {
         if (!isLoadMoreData) {
-
             searchDataAdapter = SearchDataAdapter(this, hitModelList, onItemClick = ::onItemClick)
             recyclerView.adapter = searchDataAdapter
             searchDataAdapter?.setLoadMoreListener(object : OnLoadMoreListener {
@@ -109,29 +140,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
-    // Initialing View
-    private fun initView() {
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        swipeRefreshLayout.setOnRefreshListener {
-            callRootBack?.let {
-                it.cancel()
-            }
-            hitModelList.clear()
-            isLoadMoreData=false
-            page = 1
-            searchDataAdapter?.let {
-                it.selectedItem.clear()
-                it.setMoreDataAvailable(false)
-            }
-            textViewNoInternet.visibility = View.GONE
-            setUpToolbarTitle()
-            callSearchDataAPI()
-        }
-
-    }
-
     private fun onItemClick(position: Int) {
         if (searchDataAdapter?.selectedItem?.contains(position)!!) {
             searchDataAdapter?.selectedItem?.remove(position)
@@ -143,7 +151,7 @@ class MainActivity : AppCompatActivity() {
             supportActionBar!!.title =
                 resources.getString(R.string.app_name).plus(" (").plus(searchDataAdapter?.selectedItem!!.size).plus(")")
         } else {
-            setUpToolbarTitle()
+            setToolbarTitle()
         }
 
     }
