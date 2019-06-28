449eb7eb085fd603a18eac0e180ea9612ad127fb
mvayak <info.mvayak@gmail.com>
2019-06-28 11:43:42 +0530

    Initial commit

--- app/src/main/java/com/feed/MainActivity.kt
+++ app/src/main/java/com/feed/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.feed
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
